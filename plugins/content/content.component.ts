2a23b8b6b84ff3a91dc4507dec8f9bd83d319e0d
Shaun Maher <shaun@ghanima.net>
2020-05-10 20:45:02 +1000

    Fix mis-use of the ngOnInit function that caused ExpressionChangedAfterItHasBeenCheckedError

--- plugins/content/content.component.ts
+++ plugins/content/content.component.ts
@@ -1,58 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-//import { Subscription } from "rxjs";
 import { Observable, of } from 'rxjs';
-//import { SharedModule } from 'shared';
-//import { Item, SharedService } from '../../shared/lib/shared.service';
 import { NavigationItem, NavigationService } from '../../shared/lib/navigation.service';
-import { SomeService } from '../../app/app/services/some.service';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
-  //providers: [NavigationService]
 })
 export class ContentComponent implements OnInit {
   publishedContent:Observable<string>;
-  //navigationService:NavigationService;
-  //pageTitle: Observable<string>;
-  //primaryNavigationItems: Observable<NavigationItem[]>;
-  //secondaryNavigationItems: Observable<NavigationItem[]>;
 
-  //loading = true;
-  //errors: any;
+  constructor(navigationService: NavigationService, private router: Router) {
 
-  //private queryCategories: Subscription;
-
-  constructor(navigationService: NavigationService, someService: SomeService, private router: Router) {
-    let newItem = new NavigationItem(7, "Blog");
-    navigationService.addPrimaryNavigationItem(newItem);
-    console.log(navigationService.getSecondaryNavigationItems());
-
-    //this.navigationService = navigationService;
-    //this.secondaryNavigationItems = this.navigationService.getSecondaryNavigationItems();
-    //this.pageTitle = this.navigationService.getPageTitle();
   }
 
   ngOnInit() {
     console.log("content.component: ngOnInit", this);
-
-    //let newItem = new NavigationItem(7, "Blog");
-    //this.navigationService.addSecondaryNavigationItem(newItem);
-    //console.log(this.navigationService.getPrimaryNavigationItems());
-
-    // this.queryCategories = this.apollo
-    //   .watchQuery({
-    //     query: CATEGORIES_QUERY
-    //   })
-    //   .valueChanges.subscribe(result => {
-    //     this.data = result.data;
-    //     this.loading = result.loading;
-    //     this.errors = result.errors;
-    //   });
   }
   ngOnDestroy() {
-    // this.queryCategories.unsubscribe();
+
   }
 }
